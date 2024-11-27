b5c10253206a1e866e5e7feb1704430f5fc3cbe9
Kabir <sagithedeveloper@gmail.com>
2024-11-27 16:27:58 +0530

    Added the code

---
+++ ReactJS/S10_State_setState/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Header from "./header/Header";
+import Counter from "./counter/Counter";
+import ImageC from "./imagec/ImageC";
+function App() {
+  return (
+    <div>
+      <Header />
+      <ImageC />
+    </div>
+  );
+}
+
+export default App;
