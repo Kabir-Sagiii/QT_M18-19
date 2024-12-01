934cc1f28296b48ecce87ef94b63c02cee10020b
Kabir <sagithedeveloper@gmail.com>
2024-12-01 11:50:09 +0530

    added the code

---
+++ ReactJS/S11_State/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Header from "./header/Header";
+import Profile from "./profile/Profile";
+import Profile2 from "./profile/Profile2";
+function App() {
+  return (
+    <div>
+      <Header />
+      <Profile2 />
+      <Profile />
+    </div>
+  );
+}
+
+export default App;
