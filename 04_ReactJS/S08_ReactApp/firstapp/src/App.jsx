92aff61e04444f0dcccca9b376e2bcca85d0a148
Kabir <sagithedeveloper@gmail.com>
2025-01-22 09:33:36 +0530

    Added the code

--- 04_ReactJS/S08_ReactApp/firstapp/src/App.jsx
+++ 04_ReactJS/S08_ReactApp/firstapp/src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="app">
-      <h2>Welcome to React World, Finally</h2>
+      <h2>Welcome to React Worlds</h2>
     </div>
   );
 }
